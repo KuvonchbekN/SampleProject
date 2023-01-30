41f6cff0b0ffc3327cd59847ec7c2e9a04ba2a0d
KuvonchbekN <knormakhmatov@students.wiut.uz>
2023-01-30 11:47:53 +0500

    Add project files.

---
+++ SampleProject/Controllers/BookController.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SampleProject.Models;
+
+namespace SampleProject.Controllers
+{
+    public class BookController : Controller
+    {
+        // GET: BookController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: BookController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: BookController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: BookController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Book bookInput)
+        {
+            Book book = new Book();
+            book.publisher = bookInput.publisher;
+            book.author = bookInput.author;
+            book.title = bookInput.title;
+           
+
+            if (ModelState.IsValid)
+            {
+                _context.Add(book);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(book);
+        }
+
+        // GET: BookController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: BookController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: BookController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: BookController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ SampleProject/Models/Book.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SampleProject.Models
+{
+    public class Book : IValidatableObject
+    {
+        [Required]
+        [Range(0, int.MaxValue, ErrorMessage = "Should be bigger than 0")]
+        public int id { get; set; }
+        [Required]
+        public string author { get; set; }
+        [Required]
+        public string title { get; set; }
+        [Required]
+        public string publisher { get; set; }
+        [Range(1900, 2023, ErrorMessage = "Should be in between 1900 and 2023")]
+        public int issueYear { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
